c309f66b8be26ee416a5444799ed9d2688588356
diepdau <92206593+diepdau@users.noreply.github.com>
2023-10-07 14:31:09 +0700

    Update danhsachsinhvien.py

--- Lab2/bai5/bai5/danhsachsinhvien.py
+++ Lab2/bai5/bai5/danhsachsinhvien.py
@@ -13,10 +13,10 @@ class DanhSachSV:
     def xuat(self):
         for sv in self.dssv:
             print(sv)
-    
+            
     def timSVTheoMs(self, ms: str):
         for i in range(len(self.dssv)):
-            if self.dssv[i].mssv == ms:
+            if self.dssv[i].maSo == ms:
                 return i
         else:
             return -1
@@ -26,17 +26,13 @@ class DanhSachSV:
             return [sv for sv in self.dssv if isinstance(sv, SinhVienChinhQuy)]
         return [sv for sv in self.dssv if isinstance(sv, SinhVienPhiCQ)]
 
-    # Tìm sinh viên có điểm rèn luyện từ 80 trở lên
     def timSVDiemHon80(self):
-        return [sv for sv in self.dssv  if sv["DiemRL"] >= 80]
+        return list(filter(lambda sv: isinstance(sv, SinhVienChinhQuy) and sv.DiemRL >= 80, self.dssv))
 
-    # Tìm sinh viên có trình độ cao đẳng sinh trước 15/8/1999
+
     def timSV_CaoDang_SinhTruoc(self):
-        if self.timSVTheoLoai("Cao Đẳng"):
-            return [sv for sv in self.dssv if sv["ngaySinh"] < "15/08/1999"]
-        else:
-            return -1
-    
+        return list(filter(lambda sv: isinstance(sv, SinhVienPhiCQ) and sv.TrinhDo == "Cao Đẳng" and sv.ngaySinh < datetime.strptime("15/8/1999", "%d/%m/%Y"), self.dssv))
+
     def ReadFile(self, path: str) -> None:
         line = ""
         with open(path, "r", encoding="utf8") as fileReader:
@@ -51,10 +47,3 @@ class DanhSachSV:
                         s[3], "%d/%m/%Y"), s[4], float(s[5]))
                 self.themSV(sv)
         fileReader.close()
-
-    def __str__(self):
-        s = f"{'MSSV':<16}{'HỌ VÀ TÊN':<24}{'NGÀY SINH':<20}{'TRÌNH ĐỘ (PCQ)':<20}{'RL (CQ)'}{'TGĐT (PCQ)':>15}\n"
-        "========================================================================================================\n"
-        for sv in self.dssv:
-            s += "{}\n".format(sv)
-        return s
